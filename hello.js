aec6081715dbaf7ad5c88096d12618a6e77ee2e1
sam1611 <sameer.tanksale1611@gmail.com>
2016-04-09 16:09:52 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,4 +1,8 @@
-function myFunction()
-{
-alert(“Hello Sameer Tanksale. You are Rockstar...!”)
-}
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end(“Hello Sameer Tanksale. You are Rockstar...!”);
+}).listen(8080);
+
+console.log('Server started');
